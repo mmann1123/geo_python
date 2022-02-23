108e3be20768e7863dda5cf81e00b91a93c75331
Michael Mann <mmann1123@gmail.com>
2022-02-23 00:27:52 -0500

    Create reproj_match.py

---
+++ Scripts/reproj_match.py
@@ -0,0 +1,48 @@
+from rasterio.warp import reproject, Resampling, calculate_default_transform
+import  rasterio as rio
+def reproj_match(infile, match, outfile):
+    """Reproject a file to match the shape and projection of existing raster. 
+    
+    Parameters
+    ----------
+    infile : (string) path to input file to reproject
+    match : (string) path to raster with desired shape and projection 
+    outfile : (string) path to output file tif
+    """
+    # open input
+    with rio.open(infile) as src:
+        src_transform = src.transform
+        
+        # open input to match
+        with rio.open(match) as match:
+            dst_crs = match.crs
+            
+            # calculate the output transform matrix
+            dst_transform, dst_width, dst_height = calculate_default_transform(
+                src.crs,     # input CRS
+                dst_crs,     # output CRS
+                match.width,   # input width
+                match.height,  # input height 
+                *match.bounds,  # unpacks input outer boundaries (left, bottom, right, top)
+            )
+
+        # set properties for output
+        dst_kwargs = src.meta.copy()
+        dst_kwargs.update({"crs": dst_crs,
+                           "transform": dst_transform,
+                           "width": dst_width,
+                           "height": dst_height,
+                           "nodata": 0})
+        print("Coregistered to shape:", dst_height,dst_width,'\n Affine',dst_transform)
+        # open output
+        with rio.open(outfile, "w", **dst_kwargs) as dst:
+            # iterate through bands and write using reproject function
+            for i in range(1, src.count + 1):
+                reproject(
+                    source=rio.band(src, i),
+                    destination=rio.band(dst, i),
+                    src_transform=src.transform,
+                    src_crs=src.crs,
+                    dst_transform=dst_transform,
+                    dst_crs=dst_crs,
+                    resampling=Resampling.nearest)
